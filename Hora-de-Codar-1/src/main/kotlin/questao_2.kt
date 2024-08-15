509b8f64627d2e16da248a537f6ed655abbc2f0e
MalcolnLMR <malcolnlucas@gmail.com>
2024-08-15 09:51:37 -0300

    rm all

--- Hora-de-Codar-1/src/main/kotlin/questao_2.kt
+++
@@ -1,9 +0,0 @@
-/*
-Escreva um programa em que o usuário informe o seu nome
-e exiba a mensagem "Olá, [NomeDoUsuario]".
-*/
-fun main(){
-    print("Escreva seu nome: ")
-    var username = readlnOrNull()
-    println("Olá, $username")
-}
