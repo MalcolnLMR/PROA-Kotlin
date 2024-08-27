95da27f52f6677db57ce32fea632eef89a71a98d
MalcolnLMR <malcolnlucas@gmail.com>
2024-08-27 19:41:17 -0300

    hotel-atividade-2-completo

--- hora-de-codar-6-hotel/src/main/kotlin/Customer.kt
+++ hora-de-codar-6-hotel/src/main/kotlin/Customer.kt
@@ -1,7 +1,17 @@
-class Customer {
+class Customer(main: MainApplication) {
+    companion object{
+        fun getDailyPriceModifierByAge(age: Int): Double{
+            if (age in 7..59) return 1.0
+            if (age in 0..6) return 0.0
+            if (age in 60..150) return 0.5
+            return -1.0 // On Error return -1
+        }
 
-    constructor(main: MainApplication){
-
+        fun isAgeValid(age: Int): Double{
+            if (age in 7..59) return 1.0
+            if (age in 0..6) return 0.0
+            if (age in 60..150) return 0.5
+            return -1.0 // On Error return -1
+        }
     }
-
 }
--- hora-de-codar-6-hotel/src/main/kotlin/Hotel.kt
+++ hora-de-codar-6-hotel/src/main/kotlin/Hotel.kt
@@ -13,15 +13,20 @@ class Hotel(main: MainApplication) {
 
     fun start(){
         // Declarations
-        var price: Double
-        var days: Int
-        var room: Int
-        var guest: String
+        val price: Double
+        val priceCalculated: Double
+        val days: Int
+        val room: Int
+
+        var guestFreeDailyCounter = 0
+        var guestHalfDailyCounter = 0
+        var guestFullDailyCounter = 0
+
         var read: String
 
-        /* TODO
+        /* NOTA 27/08/2024 19:10
          * Este trecho eu acabei fazendo com While(true)
-         * não é a coisa mais segura e correta de ser feita
+         * não é a coisa mais segura e correta de ser feita,
          * mas é o que tem para hoje, eventualmente eu revisito
          * isso aqui e arrumo :)
          */
@@ -42,12 +47,49 @@ class Hotel(main: MainApplication) {
 
         println("O valor de $days dia${if (days > 0) "s" else ""} de hospedagem é de R$ ${price * days}")
 
+        /* NOTA 27/08/2024 19:10
+         * Achei esquisito que ao perguntar os hóspedes, nós solicitamos o nome
+         * e não usamos esta informação para nada, mas como está desta forma no enunciado
+         * não irei alterar por hora
+         */
+
+        var auxGuest: String
+        var auxModifier: Double
         while (true){
             read = TextManager.askToUser("Qual o nome do hóspede?")
-            //if (TextManager.askToConfirmation("Confirma o nome $read?")) break
-            break
+            if (read.lowercase() == "pare") break
+            auxGuest = read
+
+            while (true){
+                read = TextManager.askToUser("Qual a idade do hóspede?")
+                if (Numbers.isNumeric(read) && read.toInt() in 0..150) break
+                println("Número inválido, ${mainApplication.getUserName()}")
+            }
+
+            auxModifier = Customer.getDailyPriceModifierByAge(read.toInt())
+            when (auxModifier){
+                0.0 -> guestFreeDailyCounter++
+                0.5 -> guestHalfDailyCounter++
+                1.0 -> guestFullDailyCounter++
+                else -> {println("Erro ao cadastrar $auxGuest"); continue}
+            }
+
+            /* NOTA
+             * Este trecho ficou um pouco complexo, vou explicar como ele funciona:
+             * o println() está pegando o nome do hóspede, e irá dividir usando os " " (espaços)
+             * e ao fazer isso, pega o primeiro valor, traduzindo, ele verifica onde está a diferença
+             * de nome e sobrenome, separa a 'string' numa lista e pega o primeiro valor.
+             * Após isso, ele verifica qual o modificador de preço usado, e dependendo do valor
+             * escreve "possui gratuidade" ou "paga meia"
+             */
+            println("$auxGuest cadastrada(o) com sucesso. ${if (auxModifier != 1.0) auxGuest.split(" ")[0] +
+                    (if (auxModifier == 0.0) " possui gratuidade" else " paga meia")
+                    else ""}")
         }
-        guest = read
+        priceCalculated = (price * guestFullDailyCounter  * days) + (price * guestHalfDailyCounter * 0.5  * days)
+        println("${mainApplication.getUserName()}, o valor total das hospedagens é: R$ ${priceCalculated}; " +
+                "${if (guestFreeDailyCounter > 0) "$guestFreeDailyCounter gratuidade(s)" else ""}; " +
+                "${if (guestHalfDailyCounter > 0) "$guestHalfDailyCounter meia(s)" else ""}; ")
 
         while (true){
             read = TextManager.askToUser("Qual quarto para reserva? (1-20)")
@@ -58,28 +100,28 @@ class Hotel(main: MainApplication) {
         }
         room = read.toInt()
 
-        /* NOTA
+        /* NOTA 27/08/2024 19:10
          * Talvez escolher o ponto de retorno para esta função seja interessante
          */
-        var read_bool: Boolean = TextManager.askToConfirmation("${mainApplication.getUserName()}, " +
-                "você confirma a hospedagem para $guest por $days dia${if (days > 0) "s" else ""} para o " +
-                "quarto $room por R$ ${price * days}?")
-        if (read_bool) {
+        val readBool: Boolean = TextManager.askToConfirmation("${mainApplication.getUserName()}, " +
+                "você confirma a hospedagem por $days dia${if (days > 0) "s" else ""} para o " +
+                "quarto $room por R$ ${priceCalculated}?")
+        if (readBool) {
             if(!rooms.addClient(room, price, days)) {
                 println("Erro ao adicionar")
             } else {
-                println("${mainApplication.getUserName()}, reserva efetuada para $guest")
+                println("${mainApplication.getUserName()}, reserva efetuada")
                 println(rooms.getRoomAvailabilityText())
             }
         }
         println("\n• Retornando ao menu •\n")
     }
 
-    fun onStart(){
+    private fun onStart(){
 
     }
 
-    fun onStop(){
+    private fun onStop(){
 
     }
 
